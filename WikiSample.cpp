684e238b428f1cff0109f4ac46fecca520192fbf
menash <galmenashofri@gmail.com>
2017-12-09 20:36:15 +0200

    Removed leftovers...

--- WikiSample.cpp
+++ WikiSample.cpp
@@ -37,54 +37,10 @@ class POutputPolicy
 
 protected:
 
-	/* Note:	Assuming you are willing to define an explicit interface,
-	 * 			you must whitelist all versions or use the WeakPolicy (see below).
-	 *
-	 * Note:	You can either whitelist only one or be required to specify all allowed types in
-	 *
-	 * TODO find a smoother whitelisting method
-	 */
-
-	// Option #1: Policy for a single data type
-	/*
-	POutputPolicy(print_p1 = &_PolicyClass::print) {}
-*/
-	// Option #2 (commented out): Whitelist multiple types in policy, choose a single one in usage
-	//		TODO POC
-	// 		typedef void (_PolicyClass::*print_p2)(char);
-	// 		POutputPolicy(print_p1 = &_PolicyClass::print<std::string const &>) {}
-	// 		POutputPolicy(print_p2 = &_PolicyClass::print<char>) {}
-
-	// Option #2.5 (commented out): Whitelist multiple types in policy, force to have all of them combined.
-	//		TODO POC
-	//		typedef void (_PolicyClass::*print_p1)(std::string const &);
-	// 		typedef void (_PolicyClass::*print_p2)(char);
-	// 		POutputPolicy(	print_p1 = &_PolicyClass::print<std::string const &>,
-	//						print_p2 = &_PolicyClass::print<char>) {}
-
 	// TODO explain: no support for the nothrow guarantee by standard definition
-
-	// Template version - allow anything
-	/*
-	template <typename T>
-	POutputPolicy(print_ps<T> = &_PolicyClass::print) {}
-*/
-
-		/**********************
-		 * REQUIRED TEMPLATE SPECIFICATIONS LIST:
-		// Each and every one of the following specifications are required
-		static_assert(std::is_same<decltype(std::declval<_PolicyClass>().print(std::declval<std::string>())), void >::value, "valid sig");
-		static_assert(std::is_same<decltype(std::declval<_PolicyClass>().print(std::declval<char>())), void >::value, "valid sig");
-
-		static_assert(std::is_same<decltype(std::declval<_PolicyClass>().x(std::declval<int>())), void >::value, "valid sig");
-		static_assert(std::is_same<decltype(std::declval<_PolicyClass>().x(std::declval<bool>())), void >::value, "valid sig");
-		static_assert(std::is_same<decltype(std::declval<_PolicyClass>().x(std::declval<Fake>())), void >::value, "valid sig");
-		*/
-
-	// Option #3: Use the weak policy - only check whether a member function with the same name exist.
-	// TODO add weak policy to lib
-	struct WeakPolicy_t { };
-	static constexpr const WeakPolicy_t WeakPolicy = {};
+	// TODO explain: White/Black-listing limitations due to language support - known issue
+	// TODO explain: Template functions limitations due to language support
+	// TODO add example for is_member_function on function name
 
 	typedef void (_PolicyClass::*print_p1)(std::string const &) const;
 
@@ -126,82 +82,8 @@ protected:
     }
 };
 
-template<typename U, typename expr_t, expr_t Expr>
-struct ClassName
-{
-private:
-template<typename>
-static constexpr std::false_type test(...);
-
-template<typename T = U>
-static decltype((Expr), std::true_type{}) test(int) ;
-
-public:
-static constexpr bool value = decltype(test<U>(0))::value;
-};
-
-
-// TODO try to create a whitelist but having combination of few of those:
-template <typename _PC>
-struct Whatever
-{
-	typedef void (_PC::*doThat_int)(int);
-	typedef void (_PC::*doThat_char)(char);
-	typedef void (_PC::*doThat_intptr)(int*);
-
-	static constexpr bool func(doThat_int) { return true; }
-
-	template <typename... T>
-	static constexpr bool func(T...) { return false; }
-};
-
-template <typename _PC>
-struct Whatever2
-{
-	typedef void (_PC::*doThat_char)(char);
-	typedef void (_PC::*doThat_int)(int);
-
-	static constexpr bool func(doThat_char) { return true; }
-
-	template <typename... T>
-	static constexpr bool func(T...) { return false; }
-};
-
-template <typename _PC>
-struct Whatever3
-{
-	typedef void (_PC::*doThat_intptr)(int*);
-	typedef void (_PC::*doThat_int)(int);
-
-	static constexpr bool func(doThat_intptr) { return true; }
-
-	template <typename... T>
-	static constexpr bool func(T...) { return false; }
-};
-
-template <typename _PC>
-struct Pol
-{
-
-	Pol()
-	{
-		printf("val: %d\n", Whatever<_PC>::func(&_PC::doThat));
-		printf("val: %d\n", Whatever2<_PC>::func(&_PC::doThat));
-
-		// TODO works only if policy class has only one overload. try to solve somehow...
-		// printf("val: %d\n", Whatever3<_PC>::func(&_PC::doThat));
-
-	}
-};
-
-struct A : Pol<A>
-{
-	void doThat(int) {}
-	void doThat(char) {}
-};
-
 template <class _PolicyClass>
-class PLanguagePolicy : STATIC_DERIVE_PUBLIC<_PolicyClass>
+class PLanguagePolicy : _PolicyClass
 {
 public:
 		// Function description...
@@ -215,6 +97,12 @@ POLICY_DECL(PLanguagePolicy2,
 			REQUIRE_CONST_FUNC(std::string, message)
 			);
 
+//REQUIRE_CONST_FUNC(void, print, std::string),
+POLICY_DECL(PoutputPolicy2,
+			REQUIRE_CONST_FUNC(void, print, const std::string&),
+			REQUIRE_CONST_FUNC(void, print, const char&),
+			REQUIRE_CONST_FUNC(void, print, const int&)
+			);
 
 template <typename OutputPolicy, typename LanguagePolicy>
 struct HelloWorld_SafeWrapped : public HelloWorld< PolicyEnforcer<POutputPolicy, OutputPolicy>, PolicyEnforcer<PLanguagePolicy2, LanguagePolicy> >
@@ -252,27 +140,6 @@ void functionThatExpects(const HelloWorld<A...>& h)
  * EDITED: 	Each paragraph in the following code section contains:
  * 			both original and wrapped version of HelloWorld Host class.
  */
-
-struct t_POLICIES_FROM_HERE {};
-static constexpr struct t_POLICIES_FROM_HERE POLICIES_FROM_HERE = {};
-
-struct Secret
-{
-protected:
-	void func()
-	{
-
-	}
-};
-
-struct WA
-{
-	static void print()
-	{
-		std::cout << std::is_same<decltype(&RefProtected<Secret, WA>::func), void (Secret::*)()>::value << std::endl;
-	}
-};
-
 int main()
 {
     /* Example 1 */
@@ -293,8 +160,6 @@ int main()
     hello_world2.run(); // prints "Hallo Welt!"
     HelloWorldGerman_SafeWrapped hello_world2_SafeWrapped;
     functionThatExpects(hello_world2_SafeWrapped); // prints "Hallo Welt!"
-
-    A wish4thebest;
 
     std::cout << std::boolalpha;
     std::cout << std::endl;
